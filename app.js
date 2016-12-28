9bb4f3614722ed3b2ecdd9cbac0c8a676a3b3695
BillTan <billhstan@yahoo.com.sg>
2016-12-28 21:16:06 +0800

    applied npm install command to install dependencies: firebase, moment, request, cors, body-parser, https copied and replaced all the code inside the app.js file modified the manifest.yml file a bit so that the Bluemix is very sure that my project is a NodeJs application

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.route('/API/MathManager/addTwoNumbers')
     .post(function(req, res) {
       var number1 = req.body.number1;
       var number2 = req.body.number2;
-      res.json({ result: (number1 + number2  ) });
+      res.json({ result: (number1 + number2 + 88 ) });
     });
 app.route('/API/MathManager/divideTwoNumbers')
     .post(function(req, res) {
